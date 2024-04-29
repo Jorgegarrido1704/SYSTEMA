04ed598937fbd8abb14639413a0006ed25a8dae1
Jorge Garrido <garridofth@outlook.com>
2024-04-29 08:21:15 -0600

    Update invetarios cvs done

--- app/Http/Controllers/InventarioController.php
+++ app/Http/Controllers/InventarioController.php
@@ -21,23 +21,42 @@ public function __invoke()    {
             return view('inventario',['value'=>$value,'cat'=>$cat]);}
 }
 public function savedataAlm(Request $request){
-$value=session('user');
-$cat=session('categoria');
-$data = $request-> input('set_Data');
-$qty = $request->input('set_Qty');
-$items=[];
-$cant=[];
-if($data){
-    $itmes[]= explode(',', $data);
-    $cant[]= explode(',', $qty);
-}
-    if(count($itmes)>0){
+    $value = session('user');
+    $cat = session('categoria');
+    $data = $request->input('set_Data');
+    $qty = $request->input('set_Qty');
+    $items = [];
+    $cant = [];
+
+    if ($data && $qty) {
+
+        $items = explode(',', $data);
+        $cant = explode(',', $qty);
+        $cant = array_map('strval', $cant);
+        $items = array_map('strval', $items);
+
+        if (count($items) > 0 && count($cant) > 0) {
+            for ($i = 0; $i < count($items); $i++) {
+                $items[$i] = str_replace(['[', ']', '"'], '', $items[$i]);
+                $cant[$i] = str_replace(['[', ']', '"'], '', $cant[$i]);
+                $cant[$i] = floatval($cant[$i]);
+                $nueva = new entSalAlamacen();
+                $nueva->item = $items[$i];
+                $nueva->Qty = $cant[$i];
+                $nueva->movimiento = 'Entrada';
+                $nueva->usuario = $value;
+                $nueva->fecha = date('d-m-Y H:i');
+
+            $nueva->save();
+        }
+
         return Redirect::to('inventario')->with('success','Se guardaron los datos exitosamente');
     }else{
         return Redirect::to('/')->with('error','No se guardaron los datos');
     }
 
-
+}
 
 }
+
 }
