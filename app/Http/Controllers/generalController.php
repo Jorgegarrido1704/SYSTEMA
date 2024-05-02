4879d266b33cc0b22530501b8e4fdc91336a556f
Jorge Garrido <garridofth@outlook.com>
2024-05-02 13:54:19 -0600

    Update general

--- app/Http/Controllers/generalController.php
+++ app/Http/Controllers/generalController.php
@@ -107,8 +107,13 @@ class generalController extends Controller
                 $registros[$i][7]=$idrow->donde;
                 $registros[$i][8]=$idrow->paro;
                 $buscartime=DB::table('timesharn')->select('loom', 'loomF')->where('wo', $idrow->wo)->first();
+                if($buscartime->loom!=NULL){
                     $registros[$i][9]=$buscartime->loom;
                     $registros[$i][10]=$buscartime->loomF;
+                }else{
+                    $registros[$i][9]="";
+                    $registros[$i][10]="";
+                }
                 $i++;
             }
 
