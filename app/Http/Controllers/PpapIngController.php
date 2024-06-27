7b8ba6250f202eddf02217ac1676ed5c4590c230
Jorge <garridofth@outlook.com>
2024-06-27 12:13:47 -0600

    updre

--- app/Http/Controllers/PpapIngController.php
+++ app/Http/Controllers/PpapIngController.php
@@ -127,7 +127,7 @@ $i=0;
                 $cronoGram[$i][12]=$fin-$fin_org;
             }else if($mescontrol<$mesFin && $mescontrol!=intval(date('m'))){
                 $cronoGram[$i][10]=$fin;
-                $cronoGram[$i][11]=0;
+                $cronoGram[$i][11]=32;
                 $cronoGram[$i][12]=$fin-$fin_org;
             }
 
