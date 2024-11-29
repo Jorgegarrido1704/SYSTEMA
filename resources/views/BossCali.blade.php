612e41f4cf57e3a430ded15ea6afdef7dac1bf34
Jorge <garridofth@outlook.com>
2024-11-29 17:23:11 -0600

    ese

--- resources/views/BossCali.blade.php
+++ resources/views/BossCali.blade.php
@@ -2,7 +2,7 @@
 
 @section('contenido')
  <!-- Page Heading -->
- <meta http-equiv="refresh" content="60">
+ <meta http-equiv="refresh" content="90">
  <style>
     table {     width: 100%;    text-align: center;  }
     td {border-bottom: solid 2px lightblue; }
