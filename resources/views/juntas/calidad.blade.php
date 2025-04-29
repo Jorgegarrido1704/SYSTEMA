d63765542f5fa01a18d058f48e0f3136653bb10e
Jorge G <jgarrido@mx.bergstrominc.com>
2025-04-29 11:54:57 -0500

    ese

--- resources/views/juntas/calidad.blade.php
+++ resources/views/juntas/calidad.blade.php
@@ -161,7 +161,7 @@
             <!-- AREAS -->
             <div class="card shadow mb-4">
                 <div class="card-header py-3">
-                    <h5 class="m-0 font-weight-bold text-primary">Top 10 employees incidents Montly</h5>
+                    <h5 class="m-0 font-weight-bold text-primary">Top 10 employees incidents Monthly</h5>
                 </div>
                 <div class="card-body" style="overflow-y: auto; height: 360px;" id="tableChange">
                     <canvas id="MonthIncidences"></canvas>
