549f8f3cf2e159866811a0589f5ac01b1796cdcf
Jorge G <jgarrido@mx.bergstrominc.com>
2025-06-04 09:08:40 -0500

    ese

--- app/Http/Controllers/juntasController.php
+++ app/Http/Controllers/juntasController.php
@@ -2075,9 +2075,7 @@ class juntasController extends Controller
     ->select('lider')
     ->distinct()
     ->where('week', '=', $week)
-    ->where($diaActual, '=', '')
-    ->whereNull($diaActual)
-
+    ->where($diaActual, '=', '-')
     ->get();
 
             $faltan= $total-($rotacion->assistencia+$rotacion->faltas+$rotacion->incapacidad+$rotacion->permisos_gose+$rotacion->permisos_sin_gose+$rotacion->vacaciones);
