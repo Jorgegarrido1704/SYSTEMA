2b378f2151fdc7bd5dd57399a62b3f8fffca3156
Jorge G <jgarrido@mx.bergstrominc.com>
2025-06-04 09:02:00 -0500

    ese

--- app/Http/Controllers/caliController.php
+++ app/Http/Controllers/caliController.php
@@ -320,12 +320,14 @@ class caliController extends generalController
             if (strpos($responsable5, ',')) {
                 $responsable5 = str_replace(',', ';', $responsable5);
             }
+            if($pn=="185-4147 " or $pn=="199-4942" or $pn=="199-6660  " or $pn=="199-3871 " or $pn=="189-6256" or $pn=="190-3559" or $pn=="185-4142"){
 
             $registroQr=DB::table('registroqrs')->where('infoQr', '=', $info)->where('CodigoIdentificaicon','=',$serial)->first();
             if (!empty($registroQr)) {
                  $registroQr=DB::table('registroqrs')->where('infoQr', '=', $info)->where('CodigoIdentificaicon','=',$serial)->delete();
             }else{
                 return redirect('calidad')->with('response', "Qr invalido");
+            }
             }
             $busquedainfo = DB::table('calidad')->select('qty', 'wo')->where('info', $info)->first();
 
