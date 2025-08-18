e367796c59ae733175ad57a2a6258069b70fbdb5
Jorge G <jgarrido@mx.bergstrominc.com>
2025-08-18 09:59:47 -0500

    ese

--- resources/views/juntas/ing.blade.php
+++ resources/views/juntas/ing.blade.php
@@ -70,7 +70,7 @@
         </div>
     </div>
     <div class="row">
-        <div class="col-xl-5 col-lg-5 mb-4">
+        <div class="col-xl-6 col-lg-6 mb-4">
             <div class="card shadow mb-4">
                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                     <h5 class="m-0 font-weight-bold text-primary">Registro De Trabajos documentacion </h5>
