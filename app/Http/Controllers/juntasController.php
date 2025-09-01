1fa8599cf831a0ba97424cbb8bb54c7be50a7455
Jorge G <jgarrido@mx.bergstrominc.com>
2025-09-01 09:15:27 -0500

    ese

--- app/Http/Controllers/juntasController.php
+++ app/Http/Controllers/juntasController.php
@@ -840,7 +840,7 @@ class juntasController extends Controller
         }
         $todayD = date('d');
         for ($i = 0; $i < $todayD; $i++)
-            if ($labelQ[$i] == 7 or $labelQ[$i] == 20 or $labelQ[$i] == 27 or $labelQ[$i] == 66) {
+            if ($labelQ[$i] == 47 or $labelQ[$i] == 40 or $labelQ[$i] == 404 or $labelQ[$i] == 66) {
                 $colorQ[$i] = 'red';
             } else {
                 $colorQ[$i] = 'green';
