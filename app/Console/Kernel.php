de2a9b6e565c862a16f9eb1d40447b1e868e6fdd
Jorge G <jgarrido@mx.bergstrominc.com>
2025-09-23 08:46:18 -0500

    modification to addweek each day

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -34,7 +34,7 @@ class Kernel extends ConsoleKernel
             ->days([1, 2, 3, 4, 5, 6])
             ->appendOutputTo(storage_path('logs/backup.log'));
             // creacion de listas de asistencia y registros
-        $schedule->job(new \App\Jobs\AddWeek())->dailyAt('07:00')->days([1, 2, 3, 4, 5]);
+        $schedule->job(new \App\Jobs\AddWeek())->dailyAt('06:30');
 
         //weekly list assistence
        $schedule->job(new \App\Jobs\reporteGeneral())->dailyAt('06:00')->days([1, 2, 3, 4, 5, 6]);
