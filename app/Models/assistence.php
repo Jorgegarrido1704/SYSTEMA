32e7772f02d91511422a847312a14762297aec2a
Jorge G <jgarrido@mx.bergstrominc.com>
2025-12-01 10:12:17 -0600

    ese

--- app/Models/assistence.php
+++ app/Models/assistence.php
@@ -2,26 +2,32 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Carbon\Carbon;
 
 class assistence extends Model
 {
     use HasFactory;
 
-    public $fillable=
-    [
-        'week', 'lider', 'name', 'lunes', 'extLunes', 'martes', 'extMartes', 'miercoles', 'extMiercoles', 'jueves',
-        'extJueves', 'viernes', 'extViernes', 'sabado', 'extSabado', 'domingo', 'extDomingo', 'bonoAsistencia', 'bonoPuntualidad',
-         'extras', 'tiempoPorTiempo', 'id_empleado','tt_lunes','tt_martes','tt_miercoles','tt_jueves','tt_viernes','tt_sabado',
-         'tt_domingo'
-    ];
+    public $fillable =
+        [
+            'week', 'lider', 'name', 'lunes', 'extLunes', 'martes', 'extMartes', 'miercoles', 'extMiercoles', 'jueves',
+            'extJueves', 'viernes', 'extViernes', 'sabado', 'extSabado', 'domingo', 'extDomingo', 'bonoAsistencia', 'bonoPuntualidad',
+            'extras', 'tiempoPorTiempo', 'id_empleado', 'tt_lunes', 'tt_martes', 'tt_miercoles', 'tt_jueves', 'tt_viernes', 'tt_sabado',
+            'tt_domingo',
+        ];
+
     protected $primaryKey = 'id';
+
     protected $keyType = 'integer';
+
     public $incrementing = true;
-    protected $table="assistence";
+
+    protected $table = 'assistence';
+
     public $timestamps = false;
+
     protected $casts = [
         'week' => 'string',
         'lider' => 'string',
@@ -53,23 +59,25 @@ class assistence extends Model
         'tt_sabado' => 'float',
         'tt_domingo' => 'float',
     ];
+
     protected $hidden = [
         'created_at',
-        'updated_at'
+        'updated_at',
     ];
-    //indexes
+    // indexes
 
-   public function scopeLeader($query,$leader)
-{
-    $week = date('W');
-    $dayNum = carbon::now()->dayOfWeek;
-    $dias =['lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo'];
-    $diaActual = $dias[$dayNum - 1];
-    if($leader == 'Admin' or $leader == 'Paola A' or $leader == 'Angy B'){
-        return $query->where('week', $week)->OrderBy($diaActual, 'ASC');
-    }else{
-    return $query->where('week', $week)->where('lider', 'LIKE', '%' . $leader.'%')->OrderBy($diaActual, 'ASC')->OrderBy($diaActual, 'ASC');
+    public function scopeLeader($query, $leader)
+    {
+        $week = date('W');
+        $dayNum = carbon::now()->dayOfWeek;
+
+        $dias = ['lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo'];
+        $diaActual = carbon::now()->dayOfWeek == '0' ? $dias[6] : $dias[$dayNum - 1];
+
+        if ($leader == 'Admin' or $leader == 'Paola A' or $leader == 'Angy B') {
+            return $query->where('week', $week)->OrderBy($diaActual, 'ASC');
+        } else {
+            return $query->where('week', $week)->where('lider', 'LIKE', '%'.$leader.'%')->OrderBy($diaActual, 'ASC')->OrderBy($diaActual, 'ASC');
         }
-}
-
+    }
 }
