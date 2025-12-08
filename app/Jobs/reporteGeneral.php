d978f95237371e08fd5da54b566c7d5896b13840
Jorge G <jgarrido@mx.bergstrominc.com>
2025-12-08 17:29:39 -0600

    ese

--- app/Jobs/reporteGeneral.php
+++ app/Jobs/reporteGeneral.php
@@ -2,14 +2,14 @@
 
 namespace App\Jobs;
 
+use App\Mail\WorkOrderReportMail;
+use App\Services\ExcelReportService;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Mail;
-use App\Mail\WorkOrderReportMail;
-use App\Services\ExcelReportService;
 
 class reporteGeneral implements ShouldQueue
 {
@@ -28,13 +28,13 @@ class reporteGeneral implements ShouldQueue
      */
     public function handle(): void
     {
-        $service = new ExcelReportService();
-            $filePath = $service->generateWorkOrderReport();
-        $senders=['jgarrido@mx.bergstrominc.com','jguillen@mx.bergstrominc.com','jgamboa@mx.bergstrominc.com','jrodriguez@mx.bergstrominc.com',
-        'vpichardo@mx.bergstrominc.com','apacheco@mx.bergstrominc.com','jcervera@mx.bergstrominc.com','lramos@mx.bergstrominc.com',
-        'emedina@mx.bergstrominc.com','drocha@mx.bergstrominc.com','enunez@mx.bergstrominc.com','dflores@mx.bergstrominc.com',
-        'eceron@mx.bergstrominc.com','ejimenez@mx.bergstrominc.com','egaona@mx.bergstrominc.com','jolaes@mx.bergstrominc.com','dvillalpando@mx.bergstrominc.com'];
-          // Mail::to('jgarrido@mx.bergstrominc.com')->send(new WorkOrderReportMail($filePath));
-           Mail::to($senders)->send(new WorkOrderReportMail($filePath));
+        $service = new ExcelReportService;
+        $filePath = $service->generateWorkOrderReport();
+        $senders = ['jamoreno@mx.bergstrominc.com', 'jguillen@mx.bergstrominc.com', 'jgamboa@mx.bergstrominc.com', 'jrodriguez@mx.bergstrominc.com',
+            'vpichardo@mx.bergstrominc.com', 'apacheco@mx.bergstrominc.com', 'jcervera@mx.bergstrominc.com', 'lramos@mx.bergstrominc.com',
+            'emedina@mx.bergstrominc.com', 'drocha@mx.bergstrominc.com', 'enunez@mx.bergstrominc.com', 'dflores@mx.bergstrominc.com',
+            'eceron@mx.bergstrominc.com', 'ejimenez@mx.bergstrominc.com', 'egaona@mx.bergstrominc.com', 'jolaes@mx.bergstrominc.com', 'dvillalpando@mx.bergstrominc.com'];
+        // Mail::to('jgarrido@mx.bergstrominc.com')->send(new WorkOrderReportMail($filePath));
+        Mail::to($senders)->send(new WorkOrderReportMail($filePath));
     }
 }
--- app/Jobs/respolados.php
+++ app/Jobs/respolados.php
@@ -2,14 +2,14 @@
 
 namespace App\Jobs;
 
+use App\Models\Wo;
+use Carbon\Carbon;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\DB;
-use Carbon\Carbon;
-
 
 class respolados implements ShouldQueue
 {
@@ -27,5 +27,35 @@ class respolados implements ShouldQueue
      * Execute the job.
      */
     public function handle(): void
-    {}
+    {
+
+        $caron = Carbon::now('America/Mexico_City');
+        $date = $caron->format('Y-m-d H:i');
+        $selectWO = Wo::where('count', '20')->get();
+
+        foreach ($selectWO as $woRecord) {
+            $cliente = $woRecord->cliente;
+            $np = $woRecord->NumPart;
+            $wo = $woRecord->wo;
+            $sono = $woRecord->po;
+            $qty = $woRecord->Qty;
+            $fechain = $woRecord->fecha;
+            $embarque = $woRecord->orday;
+            $codigo = $woRecord->info;
+
+            DB::table('retiradad')->insert([
+                'cliente' => $cliente,
+                'np' => $np,
+                'wo' => $wo,
+                'sono' => $sono,
+                'qty' => $qty,
+                'fechaing' => $fechain,
+                'fechaout' => $embarque,
+                'fecharetiro' => $date,
+                'codigo' => $codigo,
+            ]);
+        }
+        $delte = Wo::where('count', '20')->delete();
+
+    }
 }
