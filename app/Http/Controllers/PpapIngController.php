a5046e223ff57aee2c8b49e9a0df464c793ce2b8
Jorge G <jgarrido@mx.bergstrominc.com>
2026-02-19 11:16:36 -0600

    ese

--- app/Http/Controllers/PpapIngController.php
+++ app/Http/Controllers/PpapIngController.php
@@ -211,6 +211,8 @@ class PpapIngController extends Controller
             // problems in the floor
             $problem = errores::where('mostrar_ing', '=', 0)->get();
             // electical testing
+            $customers = DB::table('workschedule')->select('customer')->groupBy('customer')->get();
+            dd($customers);
 
             return view('/ing', ['ingenieros_en_piso' => $ingenieros_en_piso, 'problem' => $problem, 'paolaTdesc' => $paolaTdesc, 'alexTdesc' => $alexTdesc, 'paolaT' => $paolaT, 'alexT' => $alexT, 'alex' => $alex, 'alexDesc' => $alexDesc, 'paola' => $paola, 'paoDesc' => $paoDesc,  'fullreq' => $fullreq, 'graficasLate' => $graficasLate, 'graficOnTime' => $graficOnTime, 'cat' => $cat, 'inges' => $inges, 'value' => $value, 'enginners' => $enginners, 'answer' => $answer, 'dias_mes' => $dias_mes, 'cronoGram' => $cronoGram]);
         }
