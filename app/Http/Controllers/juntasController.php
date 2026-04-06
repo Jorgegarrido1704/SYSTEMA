1680f66bd3d01c2462f8b21b4d2210dc372bc69d
Jorge G <jgarrido@mx.bergstrominc.com>
2026-04-06 08:58:21 -0500

    ese

--- app/Http/Controllers/juntasController.php
+++ app/Http/Controllers/juntasController.php
@@ -495,15 +495,15 @@ class juntasController extends Controller
                     $query->orWhere('fecha', 'LIKE', "$date%");
                 }
             })
-            //->where('fecha', 'LIKE', "$crtl%")
+            // ->where('fecha', 'LIKE', "$crtl%")
             ->get();
         $totalb = calidadRegistro::where('codigo', 'TODO BIEN')
-            ->where('fecha', 'LIKE', "$crtl%")
-             /*       /*->where(function ($query) use ($datesToCheck) {
+           // ->where('fecha', 'LIKE', "$crtl%")
+            ->where(function ($query) use ($datesToCheck) {
                 foreach ($datesToCheck as $date) {
                     $query->orWhere('fecha', 'LIKE', "$date%");
                 }
-            })*/
+            })
             ->count();
         $totalm = count($buscarValoresMes);
         foreach ($buscarValoresMes as $rows) {
