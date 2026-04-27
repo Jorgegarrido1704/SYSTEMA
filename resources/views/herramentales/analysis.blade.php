5d2e125a41f79c8c383884dade16ca336e3e3310
Jorge G <jgarrido@mx.bergstrominc.com>
2026-04-26 22:43:08 -0500

    ese

--- resources/views/herramentales/analysis.blade.php
+++ resources/views/herramentales/analysis.blade.php
@@ -78,6 +78,19 @@
                                         </div>
                                     </div>
                                 </div>
+                                <div class="col-xl-4 col-md-6 mb-4">
+                                    <div class="card border-left-warning shadow h-100 py-2">
+                                        <div class="card-body">
+                                            <div class="row no-gutters align-items-center">
+                                                <div class="col mr-2">
+                                                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                                        Total time in setup</div>
+                                                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{round($totalTimesAVG*$totalDeTrabajos, 2)}}</div>
+                                                </div>                    
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                         <div class="col-xl-6 col-md-6 mb-4">
