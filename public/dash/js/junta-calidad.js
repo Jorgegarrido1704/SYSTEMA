bedab9e5e207ee2ed98b4171a04a78028dc8c83a
Jorge G <jgarrido@mx.bergstrominc.com>
2026-04-28 12:20:00 -0500

    ese

--- public/dash/js/junta-calidad.js
+++ public/dash/js/junta-calidad.js
@@ -37,207 +37,215 @@ var totalParetos =paretoKeys.length;
 for (var i = 0; i < totalParetos; i++) {
 totalp[i]=97;
 }
-var keys = Object.keys(datos);
-var values = Object.values(datos);
-var color=color1 = [];
+try{ //Barra fallas Calidad
+        var keys = Object.keys(datos);
+        var values = Object.values(datos);
+        var color=color1 = [];
 
-for (var i = 0; i < keys.length; i++) {
-var red = Math.floor(Math.random() * 256);
-var blue = Math.floor(Math.random() * 256);
-var green = Math.floor(Math.random() * 256);
-color.push("rgb(" + red + "," + blue + "," + green + ")");
-}
-for (var i = 0; i < keys.length; i++) {
-var red1 = Math.floor(Math.random() * 256);
-var blue1 = Math.floor(Math.random() * 256);
-var green1 = Math.floor(Math.random() * 256);
-color1.push("rgb(" + red + "," + blue + "," + green + ")");
-}
-
-var ctx1 = document.getElementById("BarCali");
-var Calibar = new Chart(ctx1, {
-type: 'bar',
-data: {
-    labels: keys,
-    datasets: [{
-        data:values,
-        backgroundColor: color,
-        hoverBackgroundColor:color1,
-        hoverBorderColor: "rgba(234, 236, 244, 1)",
-    }],
-},
-options: {
-
-    maintainAspectRatio: false,
-    tooltips: {
-        backgroundColor: "rgb(255,255,255)",
-        bodyFontColor: "#858796",
-        borderColor: '#dddfeb',
-        borderWidth: 1,
-        xPadding: 15,
-        yPadding: 15,
-        displayColors: false,
-        caretPadding: 10,
-    },
-    legend: {
-        display: false
-    },
-    scales: {
-        yAxes: [{
-            ticks: {
-                beginAtZero: true,
-                min: 0,
-                stepSize: 1,
-            },
-            gridLines: {
-                color: '#e3e3e3',
-                drawBorder: false,
-            },
-        }],
-        xAxes: [{
-            gridLines: {
-                display: false,
-            },
-        }]
-    }
-},
-});
-
- // Issues in quality
-var ctx2 = document.getElementById("pareto");
-var calidadPareto = new Chart(ctx2, {
-    type: 'line',
-    data: {
-        labels: paretoKeys,
-        datasets: [
-            {
-                label: 'FTQ',
-                data: paretoValues,
-                backgroundColor: ['#1cc88a', 'red'],
-                hoverBackgroundColor: ['#1cc89a', '#ff1a1a'],
-                borderColor: '#1cc88a',
-                fill: false, // Evita rellenar el área debajo de la línea
-                borderWidth: 4
-            },
-            {
-                label: 'Goal',
-                data: totalp,
-                borderColor: '#FFCC00',
-                borderDash: [5, 5],
-                pointRadius: 0,
-                fill: false,
-                borderWidth: 2
-            }
-        ]
-    },
-    options: {
-        maintainAspectRatio: false,
-        tooltips: {
-            backgroundColor: "rgb(255,255,255)",
-            bodyFontColor: "#858796",
-            borderColor: '#dddfeb',
-            borderWidth: 1,
-            xPadding: 15,
-            yPadding: 15,
-            displayColors: false,
-            caretPadding: 10,
-        },
-        legend: {
-            display: true
-        },
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true,
-                    min: 90,
-                    max: 100,
-                    stepSize: 1,
-                },
-                gridLines: {
-                    color: '#e3e3e3',
-                    drawBorder: false,
-                },
-            }],
-            xAxes: [{
-                gridLines: {
-                    display: false,
-                },
-            }]
+        for (var i = 0; i < keys.length; i++) {
+        var red = Math.floor(Math.random() * 256);
+        var blue = Math.floor(Math.random() * 256);
+        var green = Math.floor(Math.random() * 256);
+        color.push("rgb(" + red + "," + blue + "," + green + ")");
         }
-    }
-});
+        for (var i = 0; i < keys.length; i++) {
+        var red1 = Math.floor(Math.random() * 256);
+        var blue1 = Math.floor(Math.random() * 256);
+        var green1 = Math.floor(Math.random() * 256);
+        color1.push("rgb(" + red + "," + blue + "," + green + ")");
+        }
 
-//Bar pareto
-//console.log(pareto);
-var ctx2 = document.getElementById("barPareto");
-var calidadPareto = new Chart(ctx2, {
-    type: 'bar',
-    data: {
-        labels: pyl,
-        datasets: [
-            {
-                label: 'FTQ',
-                data: pyv,
-                backgroundColor: ['#1cc88a', 'red','yellow','blue'],
-                hoverBackgroundColor: ['#1cc89a', 'red','yellow','blue'],
-                borderColor: '#1cc88a',
-                fill: false, // Evita rellenar el área debajo de la línea
-                borderWidth: 1
-            },]
-    },
-    options: {
-        maintainAspectRatio: false,
-        tooltips: {
-            backgroundColor: "rgb(255,255,255)",
-            bodyFontColor: "#858796",
-            borderColor: '#dddfeb',
-            borderWidth: 1,
-            xPadding: 15,
-            yPadding: 15,
-            displayColors: false,
-            caretPadding: 10,
+        var ctx1 = document.getElementById("BarCali");
+        var Calibar = new Chart(ctx1, {
+        type: 'bar',
+        data: {
+            labels: keys,
+            datasets: [{
+                data:values,
+                backgroundColor: color,
+                hoverBackgroundColor:color1,
+                hoverBorderColor: "rgba(234, 236, 244, 1)",
+            }],
         },
-        responsive: true,
-        plugins: {
+        options: {
+
+            maintainAspectRatio: false,
+            tooltips: {
+                backgroundColor: "rgb(255,255,255)",
+                bodyFontColor: "#858796",
+                borderColor: '#dddfeb',
+                borderWidth: 1,
+                xPadding: 15,
+                yPadding: 15,
+                displayColors: false,
+                caretPadding: 10,
+            },
             legend: {
-                display: true, // Mostrar leyendas
-                labels: {
-                    usePointStyle: true, // Cambiar íconos de las etiquetas a cuadrados
-                    pointStyle: 'rect', // Configura los íconos a rectángulos
-                    generateLabels: function(chart) {
-                        const datasets = chart.data.datasets[0];
-                        return chart.data.labels.map((label, index) => ({
-                            text: label,
-                            fillStyle: datasets.backgroundColor[index], // Color del cuadro
-                            strokeStyle: datasets.backgroundColor[index], // Color del borde
-                            hidden: false
-                        }));
+                display: false
+            },
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true,
+                        min: 0,
+                        stepSize: 1,
+                    },
+                    gridLines: {
+                        color: '#e3e3e3',
+                        drawBorder: false,
+                    },
+                }],
+                xAxes: [{
+                    gridLines: {
+                        display: false,
+                    },
+                }]
+            }
+        },
+        });
+}catch (error) {
+    console.log("No hay fecha guardada");
+}
+
+try{ // Issues in quality
+        var ctx2 = document.getElementById("pareto");
+        var calidadPareto = new Chart(ctx2, {
+            type: 'line',
+            data: {
+                labels: paretoKeys,
+                datasets: [
+                    {
+                        label: 'FTQ',
+                        data: paretoValues,
+                        backgroundColor: ['#1cc88a', 'red'],
+                        hoverBackgroundColor: ['#1cc89a', '#ff1a1a'],
+                        borderColor: '#1cc88a',
+                        fill: false, // Evita rellenar el área debajo de la línea
+                        borderWidth: 4
+                    },
+                    {
+                        label: 'Goal',
+                        data: totalp,
+                        borderColor: '#FFCC00',
+                        borderDash: [5, 5],
+                        pointRadius: 0,
+                        fill: false,
+                        borderWidth: 2
                     }
+                ]
+            },
+            options: {
+                maintainAspectRatio: false,
+                tooltips: {
+                    backgroundColor: "rgb(255,255,255)",
+                    bodyFontColor: "#858796",
+                    borderColor: '#dddfeb',
+                    borderWidth: 1,
+                    xPadding: 15,
+                    yPadding: 15,
+                    displayColors: false,
+                    caretPadding: 10,
+                },
+                legend: {
+                    display: true
+                },
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true,
+                            min: 90,
+                            max: 100,
+                            stepSize: 1,
+                        },
+                        gridLines: {
+                            color: '#e3e3e3',
+                            drawBorder: false,
+                        },
+                    }],
+                    xAxes: [{
+                        gridLines: {
+                            display: false,
+                        },
+                    }]
                 }
             }
-        },
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true,
-                    min: 95, // Limitar mínimo
-                    max: 100, // Limitar máximo
-                    stepSize: .5, // Intervalos de 5 en 5
-                },
-                gridLines: {
-                    color: '#e3e3e3',
-                    drawBorder: false,
-                },
-            }],
-            xAxes: [{
-                gridLines: {
-                    display: false,
-                },
-            }]
-        }
-    }
-});
+        });
+}catch (error) {
+    console.log("No hay fecha guardada");
+}
 
+try{ // Pareto anual
+        var ctx2 = document.getElementById("barPareto");
+        var calidadPareto = new Chart(ctx2, {
+            type: 'bar',
+            data: {
+                labels: pyl,
+                datasets: [
+                    {
+                        label: 'FTQ',
+                        data: pyv,
+                        backgroundColor: ['#1cc88a', 'red','yellow','blue'],
+                        hoverBackgroundColor: ['#1cc89a', 'red','yellow','blue'],
+                        borderColor: '#1cc88a',
+                        fill: false, // Evita rellenar el área debajo de la línea
+                        borderWidth: 1
+                    },]
+            },
+            options: {
+                maintainAspectRatio: false,
+                tooltips: {
+                    backgroundColor: "rgb(255,255,255)",
+                    bodyFontColor: "#858796",
+                    borderColor: '#dddfeb',
+                    borderWidth: 1,
+                    xPadding: 15,
+                    yPadding: 15,
+                    displayColors: false,
+                    caretPadding: 10,
+                },
+                responsive: true,
+                plugins: {
+                    legend: {
+                        display: true, // Mostrar leyendas
+                        labels: {
+                            usePointStyle: true, // Cambiar íconos de las etiquetas a cuadrados
+                            pointStyle: 'rect', // Configura los íconos a rectángulos
+                            generateLabels: function(chart) {
+                                const datasets = chart.data.datasets[0];
+                                return chart.data.labels.map((label, index) => ({
+                                    text: label,
+                                    fillStyle: datasets.backgroundColor[index], // Color del cuadro
+                                    strokeStyle: datasets.backgroundColor[index], // Color del borde
+                                    hidden: false
+                                }));
+                            }
+                        }
+                    }
+                },
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true,
+                            min: 95, // Limitar mínimo
+                            max: 100, // Limitar máximo
+                            stepSize: .5, // Intervalos de 5 en 5
+                        },
+                        gridLines: {
+                            color: '#e3e3e3',
+                            drawBorder: false,
+                        },
+                    }],
+                    xAxes: [{
+                        gridLines: {
+                            display: false,
+                        },
+                    }]
+                }
+            }
+        });
+}catch (error) {
+    console.log("No hay fecha guardada");
+}
 // Set default font family and color
 Chart.defaults.global.defaultFontFamily = 'Nunito';
 //Q as Quality
