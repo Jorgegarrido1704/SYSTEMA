3d1e66b7e335dc1d15b9161113717055aa963d20
Jorge G <jgarrido@mx.bergstrominc.com>
2026-06-09 17:26:30 -0500

    ese

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,10 +24,10 @@ class Kernel extends ConsoleKernel
         $schedule->job(new \App\Jobs\VacacionesRegistrosJob)->cron('1 2,5,7 * * *');
 
         $schedule->job(new \App\Jobs\reportemaquinasdecorte)->everyMinute()
-            ->between('08:00', '16:30')
+            ->between('08:00', '16:35')
             ->weekdays()->saturdays() // De Lunes a Sábado
-            ->timezone('America/Mexico_City');
-             ->withoutOverlapping(10);
+            ->timezone('America/Mexico_City')
+            ->withoutOverlapping(10);
 
         // $schedule->job(new \App\Jobs\accionesCorrectivasJob)->dailyAt('07:00');
         // Data base backup
