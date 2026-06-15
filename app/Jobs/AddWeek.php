959da7340efd65e37e794fb855be307d5452130e
Jorge G <jgarrido@mx.bergstrominc.com>
2026-06-15 16:44:37 -0500

    ese

--- app/Jobs/AddWeek.php
+++ app/Jobs/AddWeek.php
@@ -80,8 +80,7 @@ class AddWeek implements ShouldQueue
                     $registro = 'N/A';
                 } elseif ($registroEmpleado->employeeNumber == 'i2047' or $registroEmpleado->employeeNumber == 'i2116'
                  or $registroEmpleado->employeeNumber == 'i2117' or $registroEmpleado->employeeNumber == 'i2214'
-                 or $registroEmpleado->employeeNumber == 'i2222' or $registroEmpleado->employeeNumber == 'i2158'
-                 or $registroEmpleado->employeeNumber == 'i2215') {
+                  or $registroEmpleado->employeeNumber == 'i2158' or $registroEmpleado->employeeNumber == 'i2215') {
                     $registro = 'HE';
                 } elseif ($registroEmpleado->tyoeWorker == 'Practicante') {
                     $registro = 'PCT';
