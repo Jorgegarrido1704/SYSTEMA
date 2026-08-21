1ae76add82bbfe0c82a101be14d3a24289272504
Jorge G <jgarrido@mx.bergstrominc.com>
2026-08-21 17:43:32 -0500

    ese

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Log;
 
 class Kernel extends ConsoleKernel
 {
@@ -19,9 +20,15 @@ class Kernel extends ConsoleKernel
         // save rotation daily
         $schedule->job(new \App\Jobs\UpdateRotacionJob)
             ->cron('31 3,5,7 * * *')
-            ->timezone('America/Mexico_City');
+            ->timezone('America/Mexico_City')
+            ->onFailure(function () {
+                Log::error('El job de rotación falló.');
+            });
 
-        $schedule->job(new \App\Jobs\VacacionesRegistrosJob)->cron('5 2,5,7,9 * * *');
+        $schedule->job(new \App\Jobs\VacacionesRegistrosJob)->cron('5 2,5,7,9 * * *')
+            ->onFailure(function () {
+                Log::error('El job de vacaciones falló.');
+            });
 
         /*  $schedule->job(new \App\Jobs\reportemaquinasdecorte)
               ->everyTenMinutes()
@@ -42,18 +49,35 @@ class Kernel extends ConsoleKernel
             ->appendOutputTo(storage_path('logs/backup.log'));
         // creacion de listas de asistencia y registros
         // $schedule->job(new \App\Jobs\AddWeek)->dailyAt('08:15');
-        $schedule->job(new \App\Jobs\AddWeek)->cron('15 5,6,7 * * *');
-
+        $schedule->job(new \App\Jobs\AddWeek)->cron('15 5,6,7 * * *')
+            ->onFailure(function () {
+                Log::error('El job de creación de listas de asistencia y registros falló.');
+            });
         // $schedule->job(new \App\Jobs\updateRoutingsTimes)->cron('1 21,22,23 * * *');
-        $schedule->job(new \App\Jobs\updateRoutingsTimes)->everyMinute(); // hourly();   // everyTenMinutes();
+        $schedule->job(new \App\Jobs\updateRoutingsTimes)->cron('0 8,13,17 * * 1-5')
+            ->timezone('America/Mexico_City')
+            ->appendOutputTo(storage_path('logs/schedule.log'))
+            ->onFailure(function () {
+                Log::error('El job de actualización de rutas falló.');
+            });
 
-        $schedule->job(new \App\Jobs\respolados)->cron('1 6,18 * * *');
+        $schedule->job(new \App\Jobs\respolados)->cron('1 6,18 * * *')
+            ->onFailure(function () {
+                Log::error('El job de respolados falló.');
+            });
+
         // weekly list assistence
-        $schedule->job(new \App\Jobs\reporteGeneral)->dailyAt('06:32');
+        $schedule->job(new \App\Jobs\reporteGeneral)->dailyAt('06:32')
+            ->onFailure(function () {
+                Log::error('El job de reporte general falló.');
+            });
         // $schedule->job(new \App\Jobs\reporteGeneral())->everyMinute()->between('07:00', '20:00');
 
         // acciones correctivas recordatorio de lunes a sabado a las 5:00 am
-        $schedule->job(new \App\Jobs\accionesCorrectivasJob)->cron('21 4,7 * * *');
+        $schedule->job(new \App\Jobs\accionesCorrectivasJob)->cron('21 4,7 * * *')
+            ->onFailure(function () {
+                Log::error('El job de acciones correctivas falló.');
+            });
     }
 
     /**
